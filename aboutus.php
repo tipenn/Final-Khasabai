36718d5b4e6e28fd60d3330ab1df736f54503abe
statice <baemina002@gmail.com>
2023-12-16 15:18:31 +0800

    abt us

--- aboutus.php
+++ aboutus.php
@@ -6,7 +6,7 @@ session_start();
 <!doctype html>
 <html lang="en">
   <head>
-  <title>KHASABAI</title>
+    <title>KHASABAI</title>
     <link rel="icon" type="image/x-icon" href="https://scontent.xx.fbcdn.net/v/t1.15752-9/369627246_289801410057308_7488894476029786008_n.png?_nc_cat=103&ccb=1-7&_nc_sid=510075&_nc_eui2=AeHREZSn9SSNmamdnSsRCcVxI9F0L_pc2Fgj0XQv-lzYWNfYyCBB0reg-zh1OLoO1jlcXtuHY_x2uQ3r8lBI6mYY&_nc_ohc=qWkxmDrrYZkAX9n5wr3&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdTlJoLVkthf50CQvYCphnSimKF4flckWwAbIeWb2MQAig&oe=657E214B">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
@@ -22,76 +22,55 @@ session_start();
 
 
     <style>
-       
+         body {
+            font-family: 'Roboto', sans-serif;
+            background-color:#F6F2EF;
+            background-repeat: no-repeat;
+            background-attachment: fixed; /* Optional, for a fixed background */
+        }
+            .navbar a {
+                transition: color 0.5s ease; /* Initial transition property for non-hover state */
+            }
+
+            .navbar a:hover {
+                font-weight: bold;
+                transition: color 0.5s ease 1s; /* Transition with a 1-second delay */
+                background: linear-gradient(to bottom, transparent 0%, transparent 90%, #83776c 90%, #83776c 100%, white);
+            }
+
+            .navbar {
+                border-top: 1px solid #ccc;
+                padding-left: 15px;
+                margin: 0px;         
+                background-color:rgb(239, 232, 227, 0.9); /* Light yellow with 90% opacity */
+        }
         .bar{
             text-align: center;
-            padding: 0px;
-            margin-top: 0;
-
-        }
-        .bar li{	
-            padding-left: 1.5em;
-            font-size: 15px;
-            font-family: Roboto;
-        
-        }
-
-    /* center container */
-        body {
-            font-family: 'Roboto', sans-serif;
-            background-color: #E8E1DA;
-            background-size: cover;
-            background-repeat: no-repeat;
-            background-attachment: fixed; 
-         /* ADD */
-        }
-        .navigation{
-            background-color: transparent;
-            }
-        .navbar a:hover {
-            background: linear-gradient(to bottom, transparent 0%, transparent 80%, #83776c 80%, #83776c 100%);
-        }
-        .navbar {
-            padding-left: 15px;
-            margin: 0px;
-        }
-    /*input with search icon */
-
-        .search .form-control {
-            padding: 0;
-            text-align: center;
-            background-color: transparent;
-            border-color: #453321;
-            border-right: none;
-            border-radius: 10px;
-            margin-left: 3.8em;
-            margin-top: 0;
-           
-        }
-
-        .search .form-control-feedback {
-            position: fixed;
-            z-index: 2;
-            width: 1px;
-            height: 2.375rem;
-            line-height: 2.375rem;
-            text-align: center;
+            padding: 2px;
+            margin-top: 2em;
+            margin-right: 0px; 
+            margin-left: 18em;
             
         }
-        .details{
-            background-color: #C5B9AF;
-            width: 70%;
-            border-radius: 3em;
-            padding: 2em;
-            margin-bottom: 3em;
-
+        .bar li{	
+            padding-left: 10px;
+            font-size: 20px;
+            font-family: Neuzeit S LT W01 Book;
+            color: #5B4E2C;
+        
         }
         h1 {
-            font-size: 50px; 
-            font-family: 'Prata', sans-serif; 
-            color:#453321; 
-            letter-spacing: 3px;
+            text-align: center;
+            font-family: 'Prata', serif; /* Set font to Prata */
+            color: #453321;
+            font-size: 60px;
+        }      
+        .float{
+            float: center;
+            margin-top:-3em;
+            position: relative;
         }
+        
 
         p.small {
             line-height: 1.4;
@@ -102,6 +81,37 @@ session_start();
             letter-spacing: 3px;
             padding-left: 1em;
             padding-right: 1em;
+            margin-top: -4em;
+        }
+        h1 {
+            font-family: 'Prata', serif;
+            font-size: 130px;
+            text-align: center;
+        }
+        h2 {
+            font-family: 'Raleway', sans-serif;
+            font-size: 45px;
+            text-align: left;
+            color: #5B4E2C;
+            margin-top: -25px;
+        }
+        h3 {
+            font-family: 'Raleway', sans-serif;
+            font-size: 30px;
+            text-align: left;
+            font-style: italic;
+            margin-top: -70px;
+        }
+
+        .ladder-text {
+            display: flex;
+            flex-direction: column;
+            align-items: flex-start;
+        }
+
+        .ladder-text span {
+            display: inline-block;
+            margin-bottom: 5px;
         }
         a {
 			text-decoration: none;
@@ -111,99 +121,130 @@ session_start();
 			color: #5B4E2C;
         }
         footer{
-            background-color: #A19284;
+            background-color:#514F4D ;
             color: white;
-            position:fixed;
             bottom: 0;
             left:0;
             width:100%;
+            height: 100px;
         }
         footer a{
-            color:#453321;
+            color:#EFE8E3;
         }
         footer a:hover{
             color: black;
         }
+        .search .form-control {
+        text-align: center;
+        background-color: transparent;
+        border-color: #453321;
+        border-radius: 10px;
+       width: 10%;
+        }
+        .search{
+            width: 240px;
+           margin-left: 6em;
+           position: relative;
+    
+        }
+        .scroll{
+            overflow-y: scroll;
+            max-height: 50em;
+        }
+        .card{
+            background-color: rgba(232, 225, 218, 0.5);
+        }
     </style>
- 
 
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Khasabai</title>
+ <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>KHASABAI</title>
+    <link rel="icon" type="image/x-icon" href="https://scontent.xx.fbcdn.net/v/t1.15752-9/369627246_289801410057308_7488894476029786008_n.png?_nc_cat=103&ccb=1-7&_nc_sid=510075&_nc_eui2=AeHREZSn9SSNmamdnSsRCcVxI9F0L_pc2Fgj0XQv-lzYWNfYyCBB0reg-zh1OLoO1jlcXtuHY_x2uQ3r8lBI6mYY&_nc_ohc=qWkxmDrrYZkAX9n5wr3&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdTlJoLVkthf50CQvYCphnSimKF4flckWwAbIeWb2MQAig&oe=657E214B">
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='main.js'></script>
 </head>
-
-    <!-- navbar -->
+<div class="navigation">
     <nav class="navbar navbar-expand-sm">
-   
-    <div class="bar">
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a href="welcome.php" class="nav-link active">
-                        Home
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="aboutus.php" class="nav-link active">
-                        About Us
-                    </a>
-                </li>
-                <li class="nav-item active">
-                    <a href="login.php" class="nav-link active">
-                        Log in
-                    </a>
-                </li>
-                <li class="nav-item active">
-                    <a href="log.php" class="nav-link active">
-                        Cart
-                    </a>
-                </li>
-            </ul>
-        </div>
-    </div>
-   
-     <!-- upper right side (nav bar) -->
-     <div style="display: flex; align-items: right;">
-            <br>
-            <hr style= "margin-top: 10px; margin-left: 15px; width: 850px; background:#4F3B33; height: 2px;"> </hr>
-        </div>
-        <br>
-        <div class="collapse navbar-collapse" id="navbarNav">
-        <div class="navbar-nav ml-auto">
         <div style="display: flex; align-items: center;">
+            <div style="margin-right: 10px; margin-left: 2em; padding-left:10px; padding-top: 6px;">
+            <img src="https://scontent.xx.fbcdn.net/v/t1.15752-9/369627246_289801410057308_7488894476029786008_n.png?_nc_cat=103&ccb=1-7&_nc_sid=510075&_nc_eui2=AeHREZSn9SSNmamdnSsRCcVxI9F0L_pc2Fgj0XQv-lzYWNfYyCBB0reg-zh1OLoO1jlcXtuHY_x2uQ3r8lBI6mYY&_nc_ohc=qWkxmDrrYZkAX9n5wr3&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdTlJoLVkthf50CQvYCphnSimKF4flckWwAbIeWb2MQAig&oe=657E214B" alt="Logo" width="70" height="70">
+            </div>
             <div>
-                <p style="margin: 0;  font-size: 18px; color:#5B4E2C ; font-family: Raleway; text-align: right;">KHASABAI</p>
-                <p style="margin: 0;  font-size: 18px; color:#5B4E2C ; font-family: Raleway; text-align: right;">BEAUTY PRODUCTS</p>
+                <p style="margin: 0;  font-size: 18px; color:#5B4E2C ; font-family: Raleway;font-weight: bold; text-align: left;">KHASABAI</p>
+                <p style="margin: 0;  font-size: 18px; color:#5B4E2C ; font-family: Raleway;font-weight: bold;  text-align: left;">BEAUTY PRODUCTS</p></a>
             </div>
+                <br>
+                <br>
                 
-            <div style="margin-left: 10px;  padding-right:10px; padding-top: 6px;">
-                <img src="https://scontent.xx.fbcdn.net/v/t1.15752-9/369627246_289801410057308_7488894476029786008_n.png?_nc_cat=103&ccb=1-7&_nc_sid=510075&_nc_eui2=AeHREZSn9SSNmamdnSsRCcVxI9F0L_pc2Fgj0XQv-lzYWNfYyCBB0reg-zh1OLoO1jlcXtuHY_x2uQ3r8lBI6mYY&_nc_ohc=qWkxmDrrYZkAX9n5wr3&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdTlJoLVkthf50CQvYCphnSimKF4flckWwAbIeWb2MQAig&oe=657E214B" alt="Logo" width="70" height="70">
-            </div>
-        </div>
-    </nav>
     </div>
-
-<!-- second navbar (search btn)-->
-<nav class="navbar navbar-expand-sm">
-    <div class="collapse navbar-collapse" id="navbarNav">
+    
+    <button type="button" data-toggle="collapse" data-target="#navbarNav" class="navbar-toggler" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon "></span>
+    </button>
+        
+    <div class="collapse navbar-collapse justify-content" id="navbarNav" >
+    <div class="bar">
+        <ul class="navbar-nav ">
+        <a href="welcome.php" class="nav-link">
+        <li class="nav-item text-center" >
+           
+                Home
+        </li>
+        </a>
+        <a href="aboutus.php" class="nav-link">
+        <li class="nav-item text-center" >
+           
+                About Us
+        </li>
+        </a>
+        <a href="log.php" class="nav-link">
+        <li class="nav-item text-center" >
+           
+                Login
+        </li>
+        </a>
+        <a href="customerprofile.php" class="nav-link">
+         <li class="nav-item text-center" >
+           
+                Profile
+         </li>
+        </a>
+        <a href="cart.php" class="nav-link">
+         <li class="nav-item text-center">
+        
+                Cart
+         </li>
+        </a>
+        
         <div class="search">
+        <form method="GET" action="" class="search">
             <div class="input-group">
-                <input type="text" class="form-control" placeholder="Search">
-                <div class="input-group-append">
-                <button class="btn btn-secondary" type="button" style="background-color: transparent; border-color: #453321; border-left: none; border-top-right-radius: 10px; border-bottom-right-radius: 10px; border-bottom-left-radius: 0; border-top-left-radius: 0;">
-                <span class="btn-label"><i class="fa-solid fa-magnifying-glass" style="color: #5a4939; "></i></span></button>
-                </button>
+                <input type="search" name="query" class="form-control" placeholder="Search..."
+                    style="border-top-left-radius: 10px; border-bottom-left-radius: 10px;">
+                
+                    <button class="btn btn-secondary" type="submit"
+                        style="background-color: transparent;width:15%; border-color: #453321; border-top-right-radius: 10px; border-bottom-right-radius: 10px; border-top-left-radius: 0; border-bottom-left-radius: 0;">
+                        <span class="btn-label"><i class="fas fa-search" style="color: #5a4939;"></i></span>
+                    </button>
+                </div>
             </div>
-        </div>
+        </form>
     </div>
-            
- </nav>
-    <br>
+</div>
+    </nav>
+        </div>
+        <br><br>
+        <div class="float">
+        <img src="assets/aboutus.png" style="width: 100%; height:800px; max-width: 100%;">
+        <h1 style="font-size: 48px; position: absolute; top: 4.9em; left: 41%; transform: translate(-10%, -40%);">Know our story.</h1><br><br>
+        <p style="font-family: 'raleway', serif; position: absolute; top: 11em; left: 35%; right: 28%; transform: translate(-10%, -40%); color: #453321; font-size: 28px; font-weight: bold; text-align: center;">
+        <span style="font-size: 22px;"><br>Elevate Your Beauty Rituals with Exquisite Beauty Products Crafted for Timeless Elegance.</span>
+        </p>
+    </div>
+    <br><br><br>
 <center>
-<div class="details">
-            <h1> ABOUT US </h1>
-            <hr>
             <p class="small"> The primary objective of this study is to assess the feasibility, benefits, and limitations of implementing an Online Skincare Purchasing System within Khasabai's enterprise procurement framework. It seeks to provide a comprehensive understanding of the potential impact of this digital approach on skincare product acquisition, aiming to guide Khasabai in making informed decisions.
             <br>
             <br> Furthermore, the study aims to analyze the current skincare procurement processes at Khasabai, identify the organization's skincare needs, and evaluate the technical infrastructure to determine its readiness for the adoption of an online system. By exploring the capabilities and limitations of this digital transformation, the study equips Khasabai with insights that will aid in optimizing skincare procurement processes, enhancing vendor relations, and ultimately ensuring the consistent delivery of high-quality skincare products to its customers.
@@ -215,28 +256,25 @@ session_start();
     </div>
     </div>
 <br>
-
  <!-- footer -->
  <footer class="container-fluid">
     <div class="row">
         <div class="col-6 text-left">
-            <p class="text-lead" style="font-size: 15px; padding-top:20px;">2023©Khasabai | <a href="" style="color: white;"> Privacy Policy</a></p>
+            <p class="text-lead" style="font-size: 18px; padding-top:2em; color: #EFE8E3;padding-left: 2em;">2023©Khasabai | Privacy Policy</p>
         </div>
-        <div class="col-md-6 text-end sm-3" style="padding-top:20px;">
+        <div class="col-md-6 text-end sm-3" style="padding-top:2em  ; ">
             <a href="mailto:your-email@example.com" class="d-inline">
-                <i class="far fa-envelope" style="margin: 5px;"></i>
+                <i class="far fa-envelope" style="margin-right: 10px; font-size: 28px;  "></i>
             </a>
             <a href="https://www.facebook.com/kylesumabat13" target="_blank" class="d-inline">
-                <i class="fab fa-facebook" style="margin: 5px;"></i>
+                <i class="fab fa-facebook" style="margin-right: 10px; font-size: 28px; "></i>
             </a>
             <a href="https://instagram.com/kharyllesumabat?igshid=NzZlODBkYWE4Ng==" target="_blank" class="d-inline">
-                <i class="fab fa-instagram" style="margin: 5px;"></i>
+                <i class="fab fa-instagram" style="margin-right: 2em; font-size: 28px; "></i>
             </a>
         </div>
     </div>
 </footer>
- </body>
-</html>
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
